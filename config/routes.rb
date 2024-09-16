00d642df53ee710ee48fdcaa80df5469dcf22bec
viveknegi111 <v_111negi@yahoo.com>
2024-09-16 19:49:26 +0530

    files updated

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   post 'v1/sign_in' => 'v1/login#create'
   post 'v1/alerts' => 'v1/alerts#create'
   get 'v1/alerts' => 'v1/alerts#index'
-  delete 'v1/:id/alerts' => 'v1/alerts#destroy'
+  delete 'v1/alerts/:id' => 'v1/alerts#destroy'
 end
